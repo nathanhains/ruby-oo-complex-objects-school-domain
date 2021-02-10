5ff77e165bcac07b4eac422c2a90ff7ffef12f2f
nathan hains <nathanhains14@yahoo.com>
2021-02-10 14:39:58 -0500

    commit tree 57d8da3134f634b10be2090ac095b71887977497

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+   
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+            if @roster.key?(grade)
+                @roster[grade] << student_name
+            else
+                @roster[grade] = []
+                @roster[grade] << student_name
+            end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.collect do |grade, students|
+            students = students.sort
+            @roster[grade] = students
+        end
+        @roster 
+    end
+
+
+end
